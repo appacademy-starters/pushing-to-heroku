9262998b47083d4f51082327d096c4ced655b70c
James Churchill <james@smashdev.com>
2020-06-03 15:38:47 -0700

    Various fixes and updates

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const imageUrl = process.env.REACT_APP_IMAGE_URL || 'http://localhost:8000';
+export const imageUrl = process.env.REACT_APP_IMAGE_URL || '';
 export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
